284a0f6804e03db543c863f1c9298b025f4b24f9
Austin Kehrer <9903austin@gmail.com>
2019-12-04 17:03:00 -0600

    Update
    *** Clears any data in outputfile per every run ***

--- MyScripts/BruteForceGen/BrutePassCrack.py
+++ MyScripts/BruteForceGen/BrutePassCrack.py
@@ -1,15 +1,18 @@
 import random
 import sys
-from io import StringIO
 from random import choice
 path = 'Outputfile.txt'
-list = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+list = "ofo"
+#list = abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
 trys = 10000000
 length = 5
+new = open(path, 'w')
+print(new)
 for i in range (0 , trys) :
+
     #Remember to add random.choice() to A in order to set the rows
     #New instances must be based on the length of the password searched for
-    A = random.choice(list), random.choice(list), random.choice(list), random.choice(list), random.choice(list)
+    A = random.choice(list), random.choice(list), random.choice(list)
     B = "".join(A)
     f = open("Outputfile.txt", "a")
     print("Try:",i , B, "Out of:", trys, file=f)

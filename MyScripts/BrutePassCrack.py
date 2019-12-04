d26965461b2d7cdecdf4dc25d0d7faa5c6b14ff4
Austin Kehrer <9903austin@gmail.com>
2019-12-04 14:49:13 -0600

    UpdatedBrutePassCrack

--- MyScripts/BrutePassCrack.py
+++ MyScripts/BrutePassCrack.py
@@ -1,16 +1,12 @@
 import random
+from random import choice
 
-    letters = [str(i) for i in range('a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p')]
-    s = [''.join([a,b,c,d,e,f,g,h]) for a in letters for b in letters for c   in letters for d in letters for e in letters for f in letters for g in letters  for h in letters]
-    random.shuffle(s)
-    real_password = 'aaaaaaaa'
-    i = 0
-
-    for code in s:
-        if code == real_password:
-            print()
-            print('The password is: ', code)
-             break
-        else:
-            i += 1
-            print(i, ' failures', end='\r')import random
+list = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+trys = 1000000
+password = "pass"
+for i in range (0 , trys) :
+    #Remember to add random.choice() to A in order to set the rows
+    #New instances must be based on the length of the password searched for
+    A = random.choice(list), random.choice(list) , random.choice(list), random.choice(list)
+    print("".join(A))
+    i += 1

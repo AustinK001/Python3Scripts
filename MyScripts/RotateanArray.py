9477547baa8246676619a406ecc2e74dba9d895e
Austin Kehrer <9903austin@gmail.com>
2019-12-11 13:16:26 -0600

    Update

--- MyScripts/RotateanArray.py
+++ MyScripts/RotateanArray.py
@@ -1,3 +1,23 @@
-def rotate(): 
-    array = [1,23,423,234]
-    return [list(x) for x in zip(*array)[::-1]]
+n = 3
+list_1 = [1, 2, 3, 4, 5, 6]
+list_1 = (list_1[len(list_1) - n:len(list_1)]+ list_1[0:len(list_1) - n])
+print(list_1)
+
+ #Print  = [4,5,6,1,2,3]
+ #Rotated to the right
+
+x = 5
+list_2 = [1,2,3,4,5,6,7,8,9,10]
+list_2 = (list_2[len(list_2) - x:len(list_2)]+ list_2[0:len(list_2) - x])
+print(list_2)
+# ^ (list_2[10 - 5:10] =
+
+c = 2
+list_3 = [1,2,3]
+list_3 = (list_3[len(list_2) - c:len(list_3)]+ list_3[0:len(list_3) - c])
+# list_3[3 - 2:len] + list_3[0:len-2] Adding indexs together to join segments of the array
+# state the array at the index of
+# the array's length
+# subtracted by the starting var
+# range through the length of the array
+# then Add a
